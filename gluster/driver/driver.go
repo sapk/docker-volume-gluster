9aa444ecc112c14e01704bd74399f273e888a3cf
Antoine GIRARD <sapk@sapk.fr>
2018-03-23 15:50:19 +0100

    more generic

--- gluster/driver/driver.go
+++ gluster/driver/driver.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/docker/go-plugins-helpers/volume"
 	"github.com/sirupsen/logrus"
-	"github.com/spf13/viper"
 )
 
 var (
@@ -25,53 +24,15 @@ type GlusterDriver = basic.Driver
 //Init start all needed deps and serve response to API call
 func Init(root string, mountUniqName bool) *GlusterDriver {
 	logrus.Debugf("Init gluster driver at %s, UniqName: %v", root, mountUniqName)
-	d := &GlusterDriver{
-		Root:          root,
-		MountUniqName: mountUniqName,
-		Persistence:   viper.New(),
-		Volumes:       make(map[string]*basic.Volume),
-		Mounts:        make(map[string]*basic.Mountpoint),
-		CfgFolder:     CfgFolder,
-		Version:       CfgVersion,
-		IsValidURI:    isValidURI,
-		MountVolume: func(d *basic.Driver, v driver.Volume, m driver.Mount, r *volume.MountRequest) (*volume.MountResponse, error) {
-			cmd := fmt.Sprintf("glusterfs %s %s", parseVolURI(v.GetRemote()), m.GetPath())
-			//cmd := fmt.Sprintf("/usr/bin/mount -t glusterfs %s %s", v.VolumeURI, m.Path)
-			//TODO fuseOpts   /usr/bin/mount -t glusterfs v.VolumeURI -o fuseOpts v.Mountpoint
-			if err := d.RunCmd(cmd); err != nil {
-				return nil, err
-			}
-			return &volume.MountResponse{Mountpoint: m.GetPath()}, nil
-		},
-	}
+	return basic.Init(root, mountUniqName, CfgFolder, CfgVersion, isValidURI, mountVolume)
+}
 
-	d.Persistence.SetDefault("volumes", map[string]*basic.Volume{})
-	d.Persistence.SetConfigName("persistence")
-	d.Persistence.SetConfigType("json")
-	d.Persistence.AddConfigPath(CfgFolder)
-	if err := d.Persistence.ReadInConfig(); err != nil { // Handle errors reading the config file
-		logrus.Warn("No persistence file found, I will start with a empty list of volume.", err)
-	} else {
-		logrus.Debug("Retrieving volume list from persistence file.")
-
-		var version int
-		err := d.Persistence.UnmarshalKey("version", &version)
-		if err != nil || version != CfgVersion {
-			logrus.Warn("Unable to decode version of persistence, %v", err)
-			d.Volumes = make(map[string]*basic.Volume)
-			d.Mounts = make(map[string]*basic.Mountpoint)
-		} else { //We have the same version
-			err := d.Persistence.UnmarshalKey("volumes", &d.Volumes)
-			if err != nil {
-				logrus.Warn("Unable to decode into struct -> start with empty list, %v", err)
-				d.Volumes = make(map[string]*basic.Volume)
-			}
-			err = d.Persistence.UnmarshalKey("mounts", &d.Mounts)
-			if err != nil {
-				logrus.Warn("Unable to decode into struct -> start with empty list, %v", err)
-				d.Mounts = make(map[string]*basic.Mountpoint)
-			}
-		}
+func mountVolume(d *basic.Driver, v driver.Volume, m driver.Mount, r *volume.MountRequest) (*volume.MountResponse, error) {
+	cmd := fmt.Sprintf("glusterfs %s %s", parseVolURI(v.GetRemote()), m.GetPath())
+	//cmd := fmt.Sprintf("/usr/bin/mount -t glusterfs %s %s", v.VolumeURI, m.Path)
+	//TODO fuseOpts   /usr/bin/mount -t glusterfs v.VolumeURI -o fuseOpts v.Mountpoint
+	if err := d.RunCmd(cmd); err != nil {
+		return nil, err
 	}
-	return d
+	return &volume.MountResponse{Mountpoint: m.GetPath()}, nil
 }
